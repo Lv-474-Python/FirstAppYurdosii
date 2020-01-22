4ddbeb3f945c012cbad0b81f8817ec73d3f04d4e
Yurii <yurdosii.ksv@gmail.com>
2020-01-22 09:51:49 +0200

    Edit c4 models

--- src/c4/models.py
+++ src/c4/models.py
@@ -1,63 +1,78 @@
 from django.conf import settings
 from django.db import models
 from django.urls import reverse
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
-User = settings.AUTH_USER_MODEL
+# User = settings.AUTH_USER_MODEL
 
 class Game(models.Model):
-    # id = models.AutoField(primary_key=True)
+    """Model representing a game history item of 2 players"""
+    
     player_1 = models.ForeignKey(
         User,
         on_delete=models.SET_NULL,
-        related_name='player_1',
+        related_name='game_player_1',
         null=True
     )
     player_2 = models.ForeignKey(
         User,
         on_delete=models.SET_NULL,
-        related_name='player_2',
+        related_name='game_player_2',
         null=True
     )
     winner = models.ForeignKey(
         User,
         on_delete=models.SET_NULL,
-        related_name='winner',
+        related_name='game_winner',
         null=True
     )
-    start = models.DateTimeField(auto_now_add=True)
+    start = models.DateTimeField()
     end = models.DateTimeField(null=True)
 
+    # Metadata
     class Meta:
+        ordering = ['-pk']
         verbose_name = "Game"
         verbose_name_plural = "Games"
 
-    def __str__(self):
-        return f'Game#{self.pk} - {self.player_1.name} vs {self.player_2.name}'
-
+    # Methods
     def get_absolute_url(self):
+        """Returns the url to access a particular instance of Game"""
         return reverse("game_detail", kwargs={"pk": self.pk})
+    
+    def __str__(self):
+        """"String for representing the Game object"""
+        return f'Game#{self.pk} - {self.player_1.name} vs {self.player_2.name}'
 
 
 class Step(models.Model):
+    """Model representing a step in game made by used"""
+
+    # Fields
     game = models.ForeignKey(
         Game,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name='steps'
     )
     user = models.ForeignKey(
         User,
         on_delete=models.SET_NULL,
+        related_name='steps',
         null=True
     )
     x = models.PositiveSmallIntegerField()
     y = models.PositiveSmallIntegerField()
 
+    # Metadata
     class Meta:
         verbose_name = "Step"
         verbose_name_plural = "Steps"
+    
+    # Methods
+    def get_absolute_url(self):
+        """Returns the url to access a particular instance of Step"""
+        return reverse("step_detail", kwargs={"pk": self.pk})
 
     def __str__(self):
+        """"String for representing the Step object"""
         return f'Game#{self.game.pk}: step#{self.pk} by {self.user.name} in [{self.x}, {self.y}]'
-
-    def get_absolute_url(self):
-        return reverse("step_detail", kwargs={"pk": self.pk})
